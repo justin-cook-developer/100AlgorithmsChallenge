8c9126a335ff4e65cb39ba5f1776cdde8c9bcfbf
Justin Cook <justin.cook.dev@gmail.com>
2019-02-28 19:07:14 -0800

    Completed add functions.

--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,14 @@
-function add(param1: number, param2: number): number {
-
+function add(param1, param2) {
+  return param1 + param2;
 }
 
-// function add2(param1: number[]): number {
+function add2(...numbers) {
+  return numbers.reduce((a, b) => a + b, 0);
+}
 
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+console.log(add(1, 2));
+console.log(add(3, 2));
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
 
