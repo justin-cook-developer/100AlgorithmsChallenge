55088c5f0b3da4c1c2eb35a0c14bb0f1faa0e343
Justin Cook <justin.cook.dev@gmail.com>
2019-03-01 08:10:22 -0800

    Completed a few more algorithms.

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,10 @@
-function addBorder(picture: string[]): string[] {
+function addBorder(picture) {
+  const length = picture[0].length;
+  const border = '*'.repeat(length + 2);
 
+  const pictureWithBorder = picture.map(line => `*${line}*`);
+
+  return [border, ...pictureWithBorder, border];
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,6 @@
-function addTwoDigits(n: any): number {
-
+function addTwoDigits(n) {
+  const[numOne, numTwo] = n.toString().split('');
+  return parseInt(numOne) + parseInt(numTwo);
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
