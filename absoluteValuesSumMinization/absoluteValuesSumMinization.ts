b001a61cfc210f0a4298282b6585cec013818dc6
Justin Cook <justin.cook.dev@gmail.com>
2019-02-28 18:58:50 -0800

    Solved algo 1.

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,11 @@
-function absoluteValuesSumMinimization(a: number[]): number {
+function absoluteValuesSumMinimization(a) {
+  const length = a.length;
+  const isEven = length % 2 === 0;
 
+  return isEven ? a[(length / 2) - 1] : a[Math.floor(length / 2)]
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
