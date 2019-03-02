cd2a80288ab3673849ec073de1206f75bb8933ee
Justin Cook <justin.cook.dev@gmail.com>
2019-03-02 09:15:39 -0800

    all longest strings: optimal sol'n

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,18 @@
-function allLongestStrings(inputArray: string[]): string[] {
+function allLongestStrings(inputArray) {
+  let longestStringLength = 0;
+  let longestStrings = [];
 
+  inputArray.forEach(elem => {
+    const length = elem.length;
+    if(length > longestStringLength) {
+      longestStringLength = length;
+      longestStrings = [elem];
+    } else if(length === longestStringLength) {
+      longestStrings.push(elem);
+    }
+  });
+
+  return longestStrings;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
