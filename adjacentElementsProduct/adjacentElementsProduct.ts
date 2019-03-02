475ee2c8e53400652dc4962a0f0e68a15cc495b5
Justin Cook <justin.cook.dev@gmail.com>
2019-03-02 08:59:07 -0800

    Refresh on use of pointers.

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,20 @@
-function adjacentElementsProduct(inputArray: number[]): number {
+function adjacentElementsProduct(inputArray) {
+  let left = 0;
+  let right = 1;
+  let largestProduct = -Infinity;
 
+  while(inputArray[right] !== undefined) {
+    const leftOperand = inputArray[left];
+    const rightOperand = inputArray[right];
+    const product = leftOperand * rightOperand;
+    if(product >= largestProduct) {
+      largestProduct = product;
+    }
+    left++;
+    right++;
+  }
+
+  return largestProduct;
 }
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
