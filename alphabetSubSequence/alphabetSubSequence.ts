4a1c08df3fa0da3b13d013bd7a130b7c64feb0c9
Justin Cook <justin.cook.dev@gmail.com>
2019-03-03 08:37:07 -0800

    Actually included alphabet subsequent solution

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,16 @@
-function alphabetSubsequence(s: string): boolean {
+function alphabetSubsequence(s) {
+  let lastCharacterCode = s.charCodeAt(0);
 
+  for(let i = 1; i < s.length; i++) {
+    const characterCode = s.charCodeAt(i);
+    if (characterCode <= lastCharacterCode) {
+      return false;
+    } else {
+      lastCharacterCode = characterCode;
+    }
+  }
+
+  return true;
 }
 
 console.log(alphabetSubsequence('zab'))
