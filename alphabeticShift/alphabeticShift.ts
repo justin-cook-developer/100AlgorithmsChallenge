619467dc174e4ea8b8b1d85550baba33e6d44646
Justin Cook <justin.cook.dev@gmail.com>
2019-03-03 08:11:21 -0800

    Alphabetic Shift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,18 @@
-function alphabeticShift(inputString: string): string {
+function shiftForward(character) {
+  const characterCode = character.charCodeAt(0);
+  if(characterCode === 122) {
+    return 'a';
+  } else {
+    return String.fromCharCode(characterCode + 1);
+  }
+}
 
+function alphabeticShift(inputString) {
+  let newString = '';
+  for(let i = 0; i < inputString.length; i++) {
+    newString += shiftForward(inputString[i]);
+  }
+  return newString;
 }
 
 console.log(alphabeticShift('crazy'));
