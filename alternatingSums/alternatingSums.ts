c899cb17cdfd7ee9d2a63f8a5279b88b466b34f3
Justin Cook <justin.cook.dev@gmail.com>
2019-03-04 09:14:11 -0800

    Initial solution for alternating sums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,11 @@
-function alternatingSums(a: number[]): number[] {
-
+function alternatingSums(a) {
+  return a.reduce(([someone, some2], currentValue, index) => {
+    if(index  % 2 === 0) {
+      return [someone + currentValue, some2];
+    } else {
+      return [someone, some2 + currentValue];
+    }
+  }, [0, 0]);
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
