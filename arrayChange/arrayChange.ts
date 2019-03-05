c70f379765d53338bf62985a0b53273159d9c06f
Justin Cook <justin.cook.dev@gmail.com>
2019-03-05 12:02:12 -0800

    Solution

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,19 @@
-function arrayChange(inputArray: number[]): number {
- 
+function arrayChange(inputArray) {
+ const copies = [...inputArray];
+ let changes = 0;
+
+ for(let i = 0; i < copies.length - 1; i++) {
+   const left = copies[i];
+   const right = copies[i + 1];
+   if (right <= left) {
+    const difference = left - right;
+    const amountToIncrement = (difference + 1)
+    changes += amountToIncrement;
+    copies[i + 1] = right + amountToIncrement;
+   }
+ }
+
+ return changes;
 }
 
-console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([2, 1, 1]));
