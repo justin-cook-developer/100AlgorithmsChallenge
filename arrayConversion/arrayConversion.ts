64aa27282115d213d9e28980f01374939e112f82
Justin Cook <justin.cook.dev@gmail.com>
2019-03-05 12:30:18 -0800

    Solution.

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,26 @@
-function arrayConversion(inputArray: number[]): number {
+const add = (a, b) => a + b;
+const multiply = (a, b) => a * b;
 
+function arrayConversion(inputArray) {
+  let toIterate = [...inputArray];
+  let iteration = 1;
+
+  while(toIterate.length > 1) {
+    const transformedArray = [];
+    const combine = iteration % 2 === 1 ? add : multiply;
+
+    for (let i = 0; i < toIterate.length; i += 2) {
+      const left = toIterate[i];
+      const right = toIterate[i + 1];
+      const result = combine(left, right) || left;
+      transformedArray.push(result);
+    }
+
+    toIterate = transformedArray;
+    iteration++;
+  }
+
+  return toIterate[0];
 }
 
-console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8, 2]));
