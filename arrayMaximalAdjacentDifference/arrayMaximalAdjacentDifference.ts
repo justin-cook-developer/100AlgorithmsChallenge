6414fdbd6d5e665d33d73781b7992aea3b524203
Justin Cook <justin.cook.dev@gmail.com>
2019-03-06 11:02:06 -0800

    Maximum adjacent difference

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,16 @@
-function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+function arrayMaximalAdjacentDifference(inputArray) {
+  let maximumDifference = -Infinity;
 
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    const left = inputArray[i];
+    const right = inputArray[i + 1];
+    const difference = Math.abs(right - left);
+    if (difference > maximumDifference) {
+      maximumDifference = difference;
+    }
+  }
+
+  return maximumDifference;
 }
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
