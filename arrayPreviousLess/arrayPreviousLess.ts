b96e53efceb56aa652133e7d8906550bdc1fdf1b
Justin Cook <justin.cook.dev@gmail.com>
2019-03-06 11:23:32 -0800

    RA previousls

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,19 @@
-function arrayPreviousLess(items: number[]): number[] {
+function getTheSmallestValue(inputArray, comparison) {
+  for (let i = inputArray.length - 1; i >= 0; i--) {
+    const element = inputArray[i];
+    if (element < comparison) {
+      return element;
+    }
+  }
+  
+  return -1;
+}
 
+function arrayPreviousLess(items) {
+  return items.map((item, i, originalArray) => {
+    const replacement = getTheSmallestValue(originalArray.slice(0, i), item);
+    return replacement;
+  });
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
