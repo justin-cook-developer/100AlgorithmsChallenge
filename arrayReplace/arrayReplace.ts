2439d594eb008953b2901d17256a9671b07398ca
Justin Cook <justin.cook.dev@gmail.com>
2019-03-07 09:52:10 -0800

    Use of map

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,5 @@
-function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+function arrayReplace(inputArray, elemToReplace, substitutionElem) {
+  return inputArray.map(element => element === elemToReplace ? substitutionElem : element);
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
