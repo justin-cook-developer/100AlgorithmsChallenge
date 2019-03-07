b1968d407aa584304ebf547d9241795edce2b820
Justin Cook <justin.cook.dev@gmail.com>
2019-03-07 10:15:25 -0800

    Solution four avoid obstacles

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,13 @@
-function avoidObstacles(inputArray: number[]): number {
+function avoidObstacles(inputArray) {
+  let jump = 1;
+  let clearingObstacles = false;
 
+  while(!clearingObstacles) {
+    jump++;
+    clearingObstacles = inputArray.every(element => element % jump !== 0);
+  }
+
+  return jump;
 }
 
 console.log(avoidObstacles([5, 3, 6, 7, 9]));
