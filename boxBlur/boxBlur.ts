575c4a3af0f1c3545ceb242aedd08f496d884bc2
Justin Cook <justin.cook.dev@gmail.com>
2019-03-08 13:28:58 -0800

    Box blur solution

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -1,7 +1,28 @@
-function boxBlur(image: number[][]): number[][] {
+function getAverage(image) {
+    const middle = Math.floor(image.length / 2);
+    const indexes = [middle - 1, middle, middle + 1];
 
+    const average = image.reduce((a, b, i) => {
+        if (indexes.includes(i)) {
+            const rowValues = b.slice(indexes[0], indexes[2] + 1).reduce((a, b) => a + b);
+            return a + rowValues;
+        }  else {
+            return a;
+        }
+    }, 0) / 9;
+
+    return Math.floor(average);
 }
 
-console.log(boxBlur([[1, 1, 1], 
-    [1, 7, 1], 
+function boxBlur(image) {
+    const average = getAverage(image);
+
+    const newImageLength = image.length - 2;
+    const newImage = Array(newImageLength).fill(Array(newImageLength).fill(average));
+
+    return newImage;
+}
+
+console.log(boxBlur([[1, 1, 1],
+    [1, 7, 1],
     [1, 1, 1]]));
