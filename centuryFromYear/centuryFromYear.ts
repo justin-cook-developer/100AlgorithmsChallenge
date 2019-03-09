095b771b45dd13398942f074dd8006d6228cbd83
Justin Cook <justin.cook.dev@gmail.com>
2019-03-09 09:07:11 -0800

    Century from year

--- centuryFromYear/centuryFromYear.ts
+++ centuryFromYear/centuryFromYear.ts
@@ -1,6 +1,10 @@
-function centuryFromYear(year: number): number {
- 
+function centuryFromYear(year) {
+  if (year % 100 === 0) {
+    return year / 100;
+  } else {
+    return Math.floor(year / 100) + 1;
+  }
 }
 
 console.log(centuryFromYear(1905));
-console.log(centuryFromYear(1700));
+console.log(centuryFromYear(5));
