467ca904facaeb21e7755f392b64a77d078bfa28
Justin Cook <justin.cook.dev@gmail.com>
2019-03-10 12:56:31 -0700

    Palindrome

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,5 +1,16 @@
-function checkPalindrome(inputString: string): boolean {
+function checkPalindrome(inputString) {
+  let left = 0;
+  let right = inputString.length - 1;
 
+  while (left <= right) {
+    if (inputString[left] !== inputString[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
 }
 
 console.log(checkPalindrome('aabaa'));
