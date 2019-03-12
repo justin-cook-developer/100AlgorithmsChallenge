6ef503aa492e37b9103f722f7be49bdf61d2eb84
Justin Cook <justin.cook.dev@gmail.com>
2019-03-12 14:03:18 -0700

    Contains close numbers

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -1,5 +1,45 @@
-function containsCloseNums(nums: number[], k: number): boolean {
+// my solution
+// const meetCondition = (indexes, k) => {
+//   let result = false;
 
+//   indexes.forEach((originalPosition, i) => {
+//     indexes.slice(i + 1).forEach(value => {
+//       if (Math.abs(originalPosition - value) <= k) {
+//         result = true;
+//       }
+//     });
+//   });
+
+//   return result;
+// }
+
+// function containsCloseNums(nums, k) {
+//   const indices = nums.reduce((accumulator, current, i) => {
+//     if (accumulator[current]) {
+//       accumulator[current] = [...accumulator[current], i];
+//     } else {
+//       accumulator[current] = [i];
+//     }
+//     return accumulator;
+//   }, {});
+
+//   return Object.values(indices)
+//     .filter(indexes => indexes.length > 1)
+//     .some(indexes => meetCondition(indexes, k));
+// }
+
+function containsCloseNums(numbers, k) {
+  for (let i = 0; i < numbers.length; i++) {
+    const first = numbers[i];
+    for(let j = i + 1; j < numbers.length; j++) {
+      const second = numbers[j];
+      if (first === second && Math.abs(i - j) <= k) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
 console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
