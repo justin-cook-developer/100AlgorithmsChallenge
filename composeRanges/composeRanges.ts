4f939d3756a39c3fae674886d473444292b8e11a
Justin Cook <justin.cook.dev@gmail.com>
2019-03-12 12:10:46 -0700

    Composed range solution

--- composeRanges/composeRanges.ts
+++ composeRanges/composeRanges.ts
@@ -1,5 +1,24 @@
-function composeRanges(nums: number[]): string[] {
+function composeRanges(nums) {
+  const ranges = [];
 
+  let start = null;
+  let end = null;
+
+  nums.forEach((current, i) => {
+    if (i === 0) {
+      start = current;
+    } else if (current === nums[i - 1] + 1) {
+      end = current;
+    } else {
+      ranges.push(start && end ? `${start}->${end}` : `${start}`);
+      start = current;
+      end = null;
+    }
+  });
+
+  ranges.push(start && end ? `${start}->${end}` : `${start}`);
+
+  return ranges;
 }
 
 
