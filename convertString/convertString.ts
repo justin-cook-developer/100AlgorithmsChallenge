abae8f38aefe4d4b2bc7e682b22d41f9a72fea75
Justin Cook <justin.cook.dev@gmail.com>
2019-03-13 09:12:12 -0700

    My convert string solution

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -1,5 +1,22 @@
-function convertString(s: string, t: string): boolean {
+function convertString(s, t) {
+  let lastPosition = s.indexOf(t[0]);
+  if (lastPosition === -1) {
+    return false;
+  }
 
+  for (let i = 1; i < t.length; i++) {
+    const position = s.indexOf(t[i]);
+    const latterPosition = s.lastIndexOf(t[i]);
+    if (position === -1) {
+      return false;
+    } else if (position <= lastPosition && latterPosition <= lastPosition) {
+      return false;
+    } else {
+      lastPosition = position <= lastPosition ? latterPosition : position;
+    }
+  }
+
+  return true;
 }
 
 console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
