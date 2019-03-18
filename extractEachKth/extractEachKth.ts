8916820159462442e70865bbd73010e95bd9b258
Justin Cook <justin.cook.dev@gmail.com>
2019-03-18 13:22:25 -0700

    Daily Algorithms

--- extractEachKth/extractEachKth.ts
+++ extractEachKth/extractEachKth.ts
@@ -1,4 +1,5 @@
-function extractEachKth(inputArray:number[], k: number): number[] {
+function extractEachKth(inputArrayd, k) {
+  return inputArrayd.filter((element, i) => (i + 1) % k !== 0);
 }
 
 console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
