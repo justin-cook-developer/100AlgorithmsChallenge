e63926481a11e290c027029d530539955fe5b398
Justin Cook <justin.cook.dev@gmail.com>
2019-03-19 17:05:40 -0700

    Daily Algos

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,24 @@
-function fancyRide(l: number, fares: number[]): string {
+const map = {
+  0: 'UberX',
+  1: 'UberXL',
+  2: 'UberPlus',
+  3: 'UberBlack',
+  4: 'UberSUV',
+};
 
+function fancyRide(l, fares) {
+  let typeOfRide = null;
+
+  for (let i = 0; i < fares.length; i++) {
+    const total = fares[i] * l;
+    if (total <=  20) {
+     typeOfRide = map[i];
+    } else {
+      break;
+    }
+  }
+
+  return typeOfRide;
 }
 
 console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
--- fareEstimator/fareEstimator.ts
+++ fareEstimator/fareEstimator.ts
@@ -1,5 +1,14 @@
-function fareEstimator(ride_time: number, ride_distance: number, cost_per_minute: number[], cost_per_mile: number[]): number[] {
+function fareEstimator(ride_time, ride_distance, cost_per_minute, cost_per_mile) {
+  const prices = [];
 
+  for(let i = 0; i < cost_per_minute.length; i++) {
+    const timeCost = ride_time * cost_per_minute[i];
+    const distanceCost = ride_distance * cost_per_mile[i];
+    const totalCost = (timeCost + distanceCost).toFixed(1);
+    prices.push(parseFloat(totalCost));
+  }
+
+  return prices;
 }
 
 console.log(fareEstimator(30, 7, [0.2, 0.35, 0.4, 0.45], [1.1, 1.8, 2.3, 3.5]));
