4be08d92cccc28ab7ca230770efa3335706c4016
Justin Cook <justin.cook.dev@gmail.com>
2019-03-22 09:01:45 -0700

    Daily Algorithms

--- houseNumbersSum/houseNumbersSum.ts
+++ houseNumbersSum/houseNumbersSum.ts
@@ -1,5 +1,6 @@
-function houseNumbersSum(inputArray: number[]): number {
-
+function houseNumbersSum(inputArray) {
+  const first0 = inputArray.indexOf(0);
+  return inputArray.slice(0, first0).reduce((a, b) => a+b);
 }
 
 console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,6 +1,23 @@
-function houseOfCats(legs: number): number[] {
+function houseOfCats(legs) {
+  const upperBound = Math.floor(legs / 2);
+  if (upperBound === 0) {
+    return [];
+  }
+  const numberHumans = [upperBound];
 
+  let catCount = 1;
+  while ((catCount * 4) < legs) {
+    const remainingLegs = legs - (catCount * 4);
+    const humans = remainingLegs / 2;
+
+    if (Number.isInteger(humans)) {
+      numberHumans.unshift(humans);
+    }
+    catCount++;
+  }
+
+  return numberHumans;
 }
 
-console.log(houseOfCats(6));
-console.log(houseOfCats(2));
+console.log(houseOfCats(1));
+console.log(houseOfCats(10));
