8e64b2862d16db8dec2a9b0f2f90f329041f0738
Justin Cook <justin.cook.dev@gmail.com>
2019-03-23 13:16:38 -0700

    HTML algorithm

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,5 +1,16 @@
-function htmlEndTagByStartTag(startTag: string): string {
+function htmlEndTagByStartTag(startTag) {
+  let tag = '';
 
+  for (let i = 1; i < startTag.length - 1; i++) {
+    const character = startTag[i];
+    if (character === ' ' || character === '>') {
+      break;
+    } else {
+      tag += character;
+    }
+  }
+
+  return `</${tag}>`;
 }
 
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
